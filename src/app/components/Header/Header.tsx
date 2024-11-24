1eb637acea6c71c3507865e73f9d4e0599161bb4
Daviti <davit.mchedlishvili.2396@gmail.com>
2024-11-24 15:05:57 +0400

    feat(add): add header, just test how app works

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='bg-red-700 text-black shadow-md w-full sticky top-0 z-50 flex justify-center items-center h-16'>Header</div>
+  )
+}
+
+
+export default Header
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import "./globals.css";
-
-
+import Header from "./components/Header/header";
 
 export const metadata: Metadata = {
   title: "Furniture",
@@ -17,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header/>
         {children}
       </body>
     </html>
