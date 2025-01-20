49226eabd64bd6d8e8b141f3fae24a88a48724d2
Daviti <davit.mchedlishvili.2396@gmail.com>
2025-01-21 01:29:41 +0400

    refr(fix) fix and change details for building the app

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,19 +1,17 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 const Hero: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null); // Reference to the video element
-  const [speed, setSpeed] = useState<number>(1); // State to track playback speed
 
   const changeSpeed = (newSpeed: number) => {
     if (videoRef.current) {
       videoRef.current.playbackRate = newSpeed; // Change the video speed
-      setSpeed(newSpeed); // Update the state
     }
   };
 
   useEffect(() => {
-    // Change the playback speed to 0.8 when the component is mounted
+    // Change the playback speed to 0.68 when the component is mounted
     changeSpeed(0.68);
   }, []);
 
@@ -31,7 +29,7 @@ const Hero: React.FC = () => {
       </video>
 
       <div className="absolute inset-0 flex top-[65%] items-center justify-center">
-        <button className="text-black border-2 border-black py-3 px-5 hover:text-lg hover:bg-white hover:py-3.5  hover:px-6 transition-all duration-500">
+        <button className="text-black border-2 border-black py-3 px-5 hover:text-lg hover:bg-white hover:py-3.5 hover:px-6 transition-all duration-500">
           Learn More
         </button>
       </div>
